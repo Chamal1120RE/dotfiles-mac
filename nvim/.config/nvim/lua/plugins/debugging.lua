4488c6d9be7a317a1c4e1678ed6fe427de10fb8b
Chamal1120 <kenway213beep@gmail.com>
2024-06-27 00:04:00 +0530

    debugpy added

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,29 +1,59 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
+  {
+      "williamboman/mason.nvim",
+      config = function()
+          require("mason").setup()
+      end,
   },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
+  {
+      "jay-babu/mason-nvim-dap.nvim",
+      config = function()
+          require("mason-nvim-dap").setup({
+              ensure_installed = { "python" },
+              automatic_installation = false, 
+              handlers = {
+                  -- Custom handlers for specific adapters can be defined here
+                  python = function(config)
+                      config.adapters = {
+                          type = "executable",
+                          command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python",
+                          args = { "-m", "debugpy.adapter" },
+                      }
+                      require('mason-nvim-dap').default_setup(config)
+                  end,
+              },
+          })
+      end,
+  },
+  {
+      "mfussenegger/nvim-dap",
+      dependencies = {
+          "rcarriga/nvim-dap-ui",
+          "nvim-neotest/nvim-nio",
+          "mfussenegger/nvim-dap-python",
+      },
+      config = function()
+          local dap = require("dap")
+          local dapui = require("dapui")
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+          require("dapui").setup()
+          require("dap-python").setup()
 
+          dap.listeners.before.attach.dapui_config = function()
+              dapui.open()
+          end
+          dap.listeners.before.launch.dapui_config = function()
+              dapui.open()
+          end
+          dap.listeners.before.event_terminated.dapui_config = function()
+              dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+              dapui.close()
+          end
 
-
-    vim.keymap.set("n", "<Leader>dt", function() dap.toggle_breakpoint() end, {})
-    vim.keymap.set("n", "<Leader>dc", function() dap.continue() end, {})
-  end,
+          vim.keymap.set("n", "<Leader>dt", function() dap.toggle_breakpoint() end, {})
+          vim.keymap.set("n", "<Leader>dc", function() dap.continue() end, {})
+      end,
+  },
 }
